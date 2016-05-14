b6be9ccb1e552f14d599c675d8c37213e809254e
kryptan <kryptan@users.noreply.github.com>
2016-05-14 19:29:11 +0300

    implement

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,47 @@
+use std::io;
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Ord, PartialOrd, Default, Debug)]
+pub struct WrittenSize(u64);
+
+impl WrittenSize {
+    pub fn new() -> WrittenSize {
+        WrittenSize(0)
+    }
+
+    pub fn size(self) -> u64 {
+        self.0
+    }
+}
+
+impl io::Write for WrittenSize {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0 += buf.len() as u64;
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::iter;
+    use std::io::Write;
+    use ::WrittenSize;
+
     #[test]
-    fn it_works() {
+    fn test_100() {
+        let max = 100;
+        let data : Vec<u8> = iter::repeat(8).take(max).collect();
+
+        for i in 0..100 {
+            let mut ws = WrittenSize::new();
+            assert!(ws.size() == 0);
+            ws.write(&data[0..i]).unwrap();
+            assert!(ws.size() == i as u64);
+            ws.write(&[0]).unwrap();
+            assert!(ws.size() == (i + 1) as u64);
+        }
     }
 }
